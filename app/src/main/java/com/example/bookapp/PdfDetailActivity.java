b67acde31e7860fd822b04e596a293c1a073d640
Huynh Bao Long <caspian.huynh@gmail.com>
2022-06-27 00:44:08 +0700

    Add PDF View Activity
    Completed by hieu nhan

--- app/src/main/java/com/example/bookapp/PdfDetailActivity.java
+++ app/src/main/java/com/example/bookapp/PdfDetailActivity.java
@@ -42,6 +42,16 @@ public class PdfDetailActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        //handle click, open to view pdf
+        binding.readBookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(PdfDetailActivity.this, PdfViewActivity.class);
+                intent1.putExtra("bookId", bookId);
+                startActivity(intent1);
+            }
+        });
     }
 
     private void loadBookDetails(){
---
+++ app/src/main/java/com/example/bookapp/PdfViewActivity.java
@@ -0,0 +1,130 @@
+package com.example.bookapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.bookapp.databinding.ActivityPdfViewBinding;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class PdfViewActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityPdfViewBinding binding;
+
+    private String bookId;
+
+    private static final String TAG = "PDF_VIEW_TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPdfViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //get bookId from intent that we passed in intent
+        Intent intent = getIntent();
+        bookId = intent.getStringExtra("bookId");
+        Log.d(TAG, "onCreate: BookId:" +bookId);
+
+        loadBookDetails();
+
+        //handle click, go back
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void loadBookDetails() {
+        Log.d(TAG, "loadBookDetails: Get Pdf URL from db....");
+        //Database Reference to get book details e.g. get book url using book id
+        //Step (1) Get Book Url using Book Id
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Books");
+        ref.child(bookId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //get book url
+                        String pdfUrl = ""+snapshot.child("url").getValue();
+                        Log.d(TAG, "onDataChange: PDF URL: "+pdfUrl);
+
+                        //Step (2) Load Pdf using that url from firebase storage
+                        loadBookFromUrl(pdfUrl);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void loadBookFromUrl(String pdfUrl) {
+        Log.d(TAG, "loadBookFromUrl: Get PDF from storage");
+        StorageReference reference = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl);
+        reference.getBytes(Constants.MAX_BYTES_PDF)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        //load pdf using bytes
+                        binding.pdfView.fromBytes(bytes)
+                                .swipeHorizontal(false)  //set false to scroll vertical, set true to swipe horizontal
+                                .onPageChange(new OnPageChangeListener() {
+                                    @Override
+                                    public void onPageChanged(int page, int pageCount) {
+                                        //set current and total pages in toolbar subtitle
+                                        int currentPage = (page + 1); //do + 1 because page starts from 0
+                                        binding.toolbarSubtitleTv.setText(currentPage + "/" +pageCount); // e.g. 3/298
+                                        Log.d(TAG, "onPageChanged: "+currentPage + "/" +pageCount);
+                                    }
+                                })
+                                .onError(new OnErrorListener() {
+                                    @Override
+                                    public void onError(Throwable t) {
+                                        Log.d(TAG, "onError: "+t.getMessage());
+                                        Toast.makeText(PdfViewActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .onPageError(new OnPageErrorListener() {
+                                    @Override
+                                    public void onPageError(int page, Throwable t) {
+                                        Log.d(TAG, "onPageError: "+t.getMessage());
+                                        Toast.makeText(PdfViewActivity.this, "Error on page "+ page +" " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .load();
+
+                        binding.progressBar.setVisibility(View.GONE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: "+e.getMessage());
+                        //failed to load book
+                        binding.progressBar.setVisibility(View.GONE);
+                    }
+                });
+
+    }
+}
